64df5dc6810dad0b8842b1347f7a6cab92c6a145
ASON123 <k10416123@gmail.com>
2015-05-20 18:06:26 +0800

    完成

--- 程式設計練習VS2012/Ch10/範例/WinTabControl.Designer.cs
+++ 程式設計練習VS2012/Ch10/範例/WinTabControl.Designer.cs
@@ -28,11 +28,203 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.rdbM = new System.Windows.Forms.RadioButton();
+            this.rdbF = new System.Windows.Forms.RadioButton();
+            this.chkMarry = new System.Windows.Forms.CheckBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtShow = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(432, 226);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabContorll_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.tabPage1.Controls.Add(this.btnAdd);
+            this.tabPage1.Controls.Add(this.chkMarry);
+            this.tabPage1.Controls.Add(this.rdbF);
+            this.tabPage1.Controls.Add(this.rdbM);
+            this.tabPage1.Controls.Add(this.txtName);
+            this.tabPage1.Controls.Add(this.txtID);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(424, 200);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "會員新增";
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.txtShow);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(424, 200);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "會員列表";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "編號";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "姓名";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "性別";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "婚姻";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(60, 33);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 22);
+            this.txtID.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(60, 67);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 22);
+            this.txtName.TabIndex = 1;
+            // 
+            // rdbM
+            // 
+            this.rdbM.AutoSize = true;
+            this.rdbM.Location = new System.Drawing.Point(60, 113);
+            this.rdbM.Name = "rdbM";
+            this.rdbM.Size = new System.Drawing.Size(35, 16);
+            this.rdbM.TabIndex = 2;
+            this.rdbM.TabStop = true;
+            this.rdbM.Text = "男";
+            this.rdbM.UseVisualStyleBackColor = true;
+            // 
+            // rdbF
+            // 
+            this.rdbF.AutoSize = true;
+            this.rdbF.Location = new System.Drawing.Point(101, 113);
+            this.rdbF.Name = "rdbF";
+            this.rdbF.Size = new System.Drawing.Size(35, 16);
+            this.rdbF.TabIndex = 2;
+            this.rdbF.TabStop = true;
+            this.rdbF.Text = "女";
+            this.rdbF.UseVisualStyleBackColor = true;
+            // 
+            // chkMarry
+            // 
+            this.chkMarry.AutoSize = true;
+            this.chkMarry.Location = new System.Drawing.Point(59, 151);
+            this.chkMarry.Name = "chkMarry";
+            this.chkMarry.Size = new System.Drawing.Size(48, 16);
+            this.chkMarry.TabIndex = 3;
+            this.chkMarry.Text = "已婚";
+            this.chkMarry.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(343, 171);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "新增";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtShow
+            // 
+            this.txtShow.Location = new System.Drawing.Point(6, 6);
+            this.txtShow.Multiline = true;
+            this.txtShow.Name = "txtShow";
+            this.txtShow.Size = new System.Drawing.Size(395, 170);
+            this.txtShow.TabIndex = 0;
+            // 
+            // WinTabControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 253);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "WinTabControl";
             this.Text = "WinTabControl";
+            this.Load += new System.EventHandler(this.WinTabControl_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.CheckBox chkMarry;
+        private System.Windows.Forms.RadioButton rdbF;
+        private System.Windows.Forms.RadioButton rdbM;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox txtShow;
+
     }
 }
--- 程式設計練習VS2012/Ch10/範例/WinTabControl.cs
+++ 程式設計練習VS2012/Ch10/範例/WinTabControl.cs
@@ -16,5 +16,63 @@ namespace 程式設計練習VS2012.Ch10.範例
         {
             InitializeComponent();
         }
+        class Employee // 定義員工類別
+        {
+            public string EmpID { get; set; }    //編號屬性
+            public string EmpName { get; set; }  //姓名屬性
+            public string EmpSex { get; set; }   //性別屬性
+            public bool EmpIsMarry { get; set; } //婚姻屬性
+        }
+        List<Employee> emp = new List<Employee>(); //建立Emp串列物件用來存放Member
+        private void WinTabControl_Load(object sender, EventArgs e)
+        {
+            txtShow.Dock = DockStyle.Fill; //txtShow 文字方塊填滿整個標籤頁
+            txtID.Font = new Font(txtShow.Font.FontFamily , 14 , FontStyle.Regular); // Show 字型大小
+            txtShow.ReadOnly = true;
+            txtShow.Multiline = true;
+            rdbF.Checked = true;  //預設男為初始扭
+
+        } 
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string sex = "";
+            if(rdbM.Checked)
+            {
+                sex = "先生";
+            }
+            else
+            {
+                sex = "小姐";
+            }
+            emp.Add(new Employee() { EmpID = txtID.Text,EmpName = txtName.Text,EmpSex = sex,EmpIsMarry = chkMarry.Checked});
+            MessageBox.Show("會員新增成功");
+            //還原預設值
+            txtID.Text = "";
+            txtName.Text = "";
+            rdbF.Checked = true;
+            chkMarry.Checked = false;
+        }
+        
+        private void tabContorll_SelectedIndexChanged(object sender,EventArgs e)
+        {
+            if(tabControl1.SelectedIndex == 1) //判斷是否切換到會員列表
+            {
+                txtShow.Text = "編號\t姓名\t性別\t是否已婚" + Environment.NewLine;
+                txtShow.Text += "=========================" + Environment.NewLine;
+                for (int i = 0; i < emp.Count;i++ )
+                {
+                    txtShow.Text += emp[i].EmpID + "\t" + emp[i].EmpName + "\t" + emp[i].EmpSex + "\t" + emp[i].EmpIsMarry.ToString() + Environment.NewLine;
+                }
+
+            }
+        }
+        private void tabPage1_Click(object sender, EventArgs e) 
+        {
+           
+        }
+
+       
+
+        
     }
 }
