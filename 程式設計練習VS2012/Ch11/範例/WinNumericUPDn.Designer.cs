e9900dc15930cbd0e15b80c1d6d8e7e22e7b0dff
ASON123 <k10416123@gmail.com>
2015-05-21 17:17:26 +0800

    ch11 WinNumericUPDn 完成

---
+++ 程式設計練習VS2012/Ch11/範例/WinNumericUPDn.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace 程式設計練習VS2012.Ch11.範例
+{
+    partial class WinNumericUPDn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblShow = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 16F);
+            this.label1.Location = new System.Drawing.Point(46, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "數字練習";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "數字";
+            // 
+            // lblShow
+            // 
+            this.lblShow.AutoSize = true;
+            this.lblShow.Location = new System.Drawing.Point(22, 104);
+            this.lblShow.Name = "lblShow";
+            this.lblShow.Size = new System.Drawing.Size(35, 12);
+            this.lblShow.TabIndex = 0;
+            this.lblShow.Text = "英文 :";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(75, 58);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // WinNumericUPDn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.lblShow);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "WinNumericUPDn";
+            this.Text = "WinNumericUPDn";
+            this.Load += new System.EventHandler(this.WinNumericUPDn_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblShow;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
---
+++ 程式設計練習VS2012/Ch11/範例/WinNumericUPDn.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 程式設計練習VS2012.Ch11.範例
+{
+    public partial class WinNumericUPDn : Form
+    {
+        public WinNumericUPDn()
+        {
+            InitializeComponent();
+        }
+        //宣告num的陣列用來存放數字和英文單字
+        string[] num = new string[] { "one", "tow", "three", "four", "five", "six", "seven", "eight", "ning", "ten" };
+        private void WinNumericUPDn_Load(object sender, EventArgs e)
+        {
+            numericUpDown1.Minimum = 1; //最小值為1
+            numericUpDown1.Maximum = 10; //最大值為10
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            int n = int.Parse(numericUpDown1.Value.ToString()) -1; //將數字上下鈕-1時即取得num陣列的索引
+            lblShow.Text = "英文 : " + num[n]; //顯示結果
+        }
+
+    }
+}
