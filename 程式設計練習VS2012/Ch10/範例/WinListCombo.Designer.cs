fa49fb69c51f412e5773ed4390fce8b94755c171
ASON123 <k10416123@gmail.com>
2015-05-21 13:35:29 +0800

    OK

--- 程式設計練習VS2012/Ch10/範例/WinListCombo.Designer.cs
+++ 程式設計練習VS2012/Ch10/範例/WinListCombo.Designer.cs
@@ -28,11 +28,231 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cboName = new System.Windows.Forms.ComboBox();
+            this.cboyear = new System.Windows.Forms.ComboBox();
+            this.cboMonth = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cboDay = new System.Windows.Forms.ComboBox();
+            this.rdbM = new System.Windows.Forms.RadioButton();
+            this.rdbF = new System.Windows.Forms.RadioButton();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lstJob = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 16F);
+            this.label1.Location = new System.Drawing.Point(86, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "員工基本登入作業";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "姓名";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "生日";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "性別";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(50, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "職業";
+            // 
+            // cboName
+            // 
+            this.cboName.FormattingEnabled = true;
+            this.cboName.Location = new System.Drawing.Point(90, 44);
+            this.cboName.Name = "cboName";
+            this.cboName.Size = new System.Drawing.Size(121, 20);
+            this.cboName.TabIndex = 1;
+            this.cboName.SelectedIndexChanged += new System.EventHandler(this.cboName_SelectedIndexChanged);
+            // 
+            // cboyear
+            // 
+            this.cboyear.FormattingEnabled = true;
+            this.cboyear.Location = new System.Drawing.Point(89, 75);
+            this.cboyear.Name = "cboyear";
+            this.cboyear.Size = new System.Drawing.Size(60, 20);
+            this.cboyear.TabIndex = 1;
+            // 
+            // cboMonth
+            // 
+            this.cboMonth.FormattingEnabled = true;
+            this.cboMonth.Location = new System.Drawing.Point(178, 75);
+            this.cboMonth.Name = "cboMonth";
+            this.cboMonth.Size = new System.Drawing.Size(60, 20);
+            this.cboMonth.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(155, 78);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 12);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "年";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(244, 78);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 12);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "月";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(333, 78);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 12);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "日";
+            // 
+            // cboDay
+            // 
+            this.cboDay.FormattingEnabled = true;
+            this.cboDay.Location = new System.Drawing.Point(267, 75);
+            this.cboDay.Name = "cboDay";
+            this.cboDay.Size = new System.Drawing.Size(60, 20);
+            this.cboDay.TabIndex = 1;
+            // 
+            // rdbM
+            // 
+            this.rdbM.AutoSize = true;
+            this.rdbM.Location = new System.Drawing.Point(90, 110);
+            this.rdbM.Name = "rdbM";
+            this.rdbM.Size = new System.Drawing.Size(35, 16);
+            this.rdbM.TabIndex = 2;
+            this.rdbM.TabStop = true;
+            this.rdbM.Text = "男";
+            this.rdbM.UseVisualStyleBackColor = true;
+            // 
+            // rdbF
+            // 
+            this.rdbF.AutoSize = true;
+            this.rdbF.Location = new System.Drawing.Point(138, 110);
+            this.rdbF.Name = "rdbF";
+            this.rdbF.Size = new System.Drawing.Size(35, 16);
+            this.rdbF.TabIndex = 2;
+            this.rdbF.TabStop = true;
+            this.rdbF.Text = "女";
+            this.rdbF.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(335, 146);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "新增";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(335, 203);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "結束";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lstJob
+            // 
+            this.lstJob.FormattingEnabled = true;
+            this.lstJob.ItemHeight = 12;
+            this.lstJob.Location = new System.Drawing.Point(91, 146);
+            this.lstJob.Name = "lstJob";
+            this.lstJob.Size = new System.Drawing.Size(120, 88);
+            this.lstJob.TabIndex = 5;
+            // 
+            // WinListCombo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 252);
+            this.Controls.Add(this.lstJob);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.rdbF);
+            this.Controls.Add(this.rdbM);
+            this.Controls.Add(this.cboDay);
+            this.Controls.Add(this.cboMonth);
+            this.Controls.Add(this.cboyear);
+            this.Controls.Add(this.cboName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Name = "WinListCombo";
             this.Text = "WinListCombo";
+            this.Load += new System.EventHandler(this.WinListCombo_Load);
+            this.StyleChanged += new System.EventHandler(this.cboName_SelectedIndexChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cboName;
+        private System.Windows.Forms.ComboBox cboyear;
+        private System.Windows.Forms.ComboBox cboMonth;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox cboDay;
+        private System.Windows.Forms.RadioButton rdbM;
+        private System.Windows.Forms.RadioButton rdbF;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ListBox lstJob;
     }
 }
--- 程式設計練習VS2012/Ch10/範例/WinListCombo.cs
+++ 程式設計練習VS2012/Ch10/範例/WinListCombo.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace 程式設計練習VS2012.Ch10.範例
 {
     public partial class WinListCombo : Form
@@ -16,5 +17,114 @@ namespace 程式設計練習VS2012.Ch10.範例
         {
             InitializeComponent();
         }
+        class Member
+        {
+            public string Name { get; set; }             //姓名屬性
+            public DateTime BirthDay { get; set; }      // 生日屬性
+            public string sex { get; set; }             //性別屬性
+            public string Job { get; set; }             //職業屬性
+        }
+        List<Member> m = new List<Member>();            //建立m 串列物件用來存放Member 會員物件
+        private void WinListCombo_Load(object sender, EventArgs e)
+        {
+            cboyear.Text = (DateTime.Now.Year - 100).ToString(); // 年下拉式清單預設值為100年前
+            cboMonth.Text = ("1"); // 月下拉式清單預設值為1
+            cboDay.Text = ("1"); //  日下拉式清單預設值為1
+            for (int i = DateTime.Now.Year  -100; i <= DateTime.Now.Year; i++ ) //下拉式清單的範圍100年前~今年
+            {
+                cboyear.Items.Add(i.ToString());
+            }
+            for (int i = 1; i < 12; i++)
+            {
+                cboMonth.Items.Add(i.ToString());
+            }
+            for (int i = 1; i < 31; i++ )
+            {
+                cboDay.Items.Add(i.ToString());
+            }
+            rdbM.Checked = true; //男選項鈕預設被選取
+            string[] Job = new string[] { "學生","公教","服務","製造","家管","其他"};
+            lstJob.Items.AddRange(Job);
+            lstJob.SelectedIndex = 0;
+            
+            
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if(cboName.Text == "") //檢查姓名是否為空字串
+            {
+                MessageBox.Show("請輸入姓名");
+                return;  //離開事件
+            }
+            bool find = false; 
+            for(int i = 0; i < m.Count;i++)
+            {
+                if(m[i].Name == cboName.Text)
+                {
+                    find = true; //如果有找到則find設為true
+                    break;
+                }
+            }
+            if(find) //若find為true 表示有該筆資料
+            {
+                MessageBox.Show("資料已存在");
+                return; 
+            }
+            else
+            {
+                DateTime MyBirthDay = new DateTime();
+                try
+                {
+                    MyBirthDay = DateTime.Parse(cboyear.Text + "/" + cboMonth.Text + "/" + cboDay.Text );
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("生日有誤!!!");
+                    MessageBox.Show(ex.ToString());
+                    return; //如果生日有誤離開此事件!!
+                }
+                cboName.Items.Add(cboName.Text);
+
+                m.Add(new Member()
+                {
+                    Name = cboName.Text,
+                    BirthDay = MyBirthDay,
+                    sex = rdbF.Checked ? "男" : "女",
+                    Job = lstJob.SelectedIndex.ToString()
+                });
+                MessageBox.Show("會員新增成功");
+            }
+        }
+
+        private void cboName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int index = cboName.SelectedIndex;
+                cboyear.Text = m[index].BirthDay.Year.ToString();
+                cboMonth.Text = m[index].BirthDay.Month.ToString();
+                cboDay.Text = m[index].BirthDay.Day.ToString();
+                if (m[index].sex == "男")
+                {
+                    rdbM.Checked = true;
+                }
+                else
+                {
+                    rdbF.Checked = true;
+                }
+                int JobIndex = lstJob.FindString(m[index].Job);
+                lstJob.SelectedIndex = JobIndex;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
     }
 }
