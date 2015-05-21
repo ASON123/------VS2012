af05727a63349033a3dada510ba405e001bec807
ASON123 <k10416123@gmail.com>
2015-05-21 17:18:00 +0800

    ch11 WinTrackBar 完成

---
+++ 程式設計練習VS2012/Ch11/範例/WinTrackBar.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace 程式設計練習VS2012.Ch11.範例
+{
+    partial class WinTrackBar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 12);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(135, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(153, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 196);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // WinTrackBar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "WinTrackBar";
+            this.Text = "WinTrackBar";
+            this.Load += new System.EventHandler(this.WinTrackBar_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ 程式設計練習VS2012/Ch11/範例/WinTrackBar.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 程式設計練習VS2012.Ch11.範例
+{
+    public partial class WinTrackBar : Form
+    {
+        public WinTrackBar()
+        {
+            InitializeComponent();
+        }
+        string[] photo = new string[] { "田園", "雪山", "山林", "月亮" };
+        private void WinTrackBar_Load(object sender, EventArgs e)
+        {
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage; //圖片隨著控制大小而伸縮
+            pictureBox1.Image = new Bitmap(photo[0] + ".jpg"); //圖片空置顯示photo[0] 元素的圖檔
+            label1.Text = "圖片名稱 :" + photo[0];
+            trackBar1.Minimum = 0;
+            trackBar1.Maximum = photo.GetUpperBound(0);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            int index = trackBar1.Value;
+            pictureBox1.Image = new Bitmap(photo[index] + ".jpg");
+            label1.Text = "圖片名稱 : " + photo[index];
+        }
+    }
+}
