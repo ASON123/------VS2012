a8217ecb369aeb81dbc6bc18ab910043d6822644
ASON123 <k10416123@gmail.com>
2015-05-21 17:30:43 +0800

    ch11 winpictureBox 完成

---
+++ 程式設計練習VS2012/Ch11/範例/winpictureBox.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace 程式設計練習VS2012.Ch11.範例
+{
+    partial class winpictureBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboPhotoName = new System.Windows.Forms.ComboBox();
+            this.pic = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "圖片名稱 :";
+            // 
+            // cboPhotoName
+            // 
+            this.cboPhotoName.FormattingEnabled = true;
+            this.cboPhotoName.Location = new System.Drawing.Point(77, 16);
+            this.cboPhotoName.Name = "cboPhotoName";
+            this.cboPhotoName.Size = new System.Drawing.Size(121, 20);
+            this.cboPhotoName.TabIndex = 1;
+            this.cboPhotoName.SelectedIndexChanged += new System.EventHandler(this.cboPhotoName_SelectedIndexChanged);
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(4, 48);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(268, 202);
+            this.pic.TabIndex = 2;
+            this.pic.TabStop = false;
+            // 
+            // winpictureBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.cboPhotoName);
+            this.Controls.Add(this.label1);
+            this.Name = "winpictureBox";
+            this.Text = "winpictureBox";
+            this.Load += new System.EventHandler(this.winpictureBox_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboPhotoName;
+        private System.Windows.Forms.PictureBox pic;
+    }
+}
---
+++ 程式設計練習VS2012/Ch11/範例/winpictureBox.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 程式設計練習VS2012.Ch11.範例
+{
+    public partial class winpictureBox : Form
+    {
+        public winpictureBox()
+        {
+            InitializeComponent();
+        }
+        
+        private void winpictureBox_Load(object sender, EventArgs e)
+        {
+            string[] photo = new string[] { "田園", "雪山", "山林", "月亮" };
+            cboPhotoName.Items.AddRange(photo);
+            cboPhotoName.SelectedIndex = 0;
+            pic.BorderStyle = BorderStyle.Fixed3D;
+            pic.SizeMode = PictureBoxSizeMode.StretchImage;
+
+        }
+
+        private void cboPhotoName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pic.Image = new Bitmap(cboPhotoName.Text + ".jpg");
+        }
+    }
+}
