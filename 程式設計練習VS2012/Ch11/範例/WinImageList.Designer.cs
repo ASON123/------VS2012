b615dd0a5ee41482cec041dd99de614be8fd39f5
ASON123 <k10416123@gmail.com>
2015-05-21 15:40:11 +0800

    OK

---
+++ 程式設計練習VS2012/Ch11/範例/WinImageList.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace 程式設計練習VS2012.Ch11.範例
+{
+    partial class WinImageList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblShow = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnFist = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblShow
+            // 
+            this.lblShow.AutoSize = true;
+            this.lblShow.Location = new System.Drawing.Point(12, 9);
+            this.lblShow.Name = "lblShow";
+            this.lblShow.Size = new System.Drawing.Size(33, 12);
+            this.lblShow.TabIndex = 0;
+            this.lblShow.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 208);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnFist
+            // 
+            this.btnFist.Location = new System.Drawing.Point(230, 61);
+            this.btnFist.Name = "btnFist";
+            this.btnFist.Size = new System.Drawing.Size(75, 23);
+            this.btnFist.TabIndex = 2;
+            this.btnFist.Text = "第一張";
+            this.btnFist.UseVisualStyleBackColor = true;
+            this.btnFist.Click += new System.EventHandler(this.btnFist_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(230, 104);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(75, 23);
+            this.btnPrev.TabIndex = 2;
+            this.btnPrev.Text = "上一張";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(230, 153);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "下一張";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(230, 204);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(75, 23);
+            this.btnLast.TabIndex = 2;
+            this.btnLast.Text = "最末張";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // WinImageList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 262);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.btnFist);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblShow);
+            this.Name = "WinImageList";
+            this.Text = "WinImageList";
+            this.Load += new System.EventHandler(this.WinImageList_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblShow;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnFist;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
---
+++ 程式設計練習VS2012/Ch11/範例/WinImageList.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 程式設計練習VS2012.Ch11.範例
+{
+    public partial class WinImageList : Form
+    {
+        public WinImageList()
+        {
+            InitializeComponent();
+        }
+
+        int num = 0; //宣告num 整數用來紀錄目前的圖片索引編號,0表示第一張
+        int i = 0;
+        string[] photo = new string[] { "田園", "雪山", "山林", "月亮" };
+        void ShowPic()
+        {
+            pictureBox1.Image = imageList1.Images[num];
+            lblShow.Text = "圖片名稱 : " + photo[num];
+        }
+
+        private void WinImageList_Load(object sender, EventArgs e)
+        {
+
+            for (i = 0; i <= photo.GetUpperBound(0);i++ )
+            {
+                imageList1.Images.Add(new Bitmap(photo[i] + ".jpg"));
+            }
+            imageList1.ImageSize = new Size(250,180);
+            imageList1.ColorDepth = ColorDepth.Depth32Bit;
+            ShowPic();
+        }
+
+        private void btnFist_Click(object sender, EventArgs e)//第一張
+        {
+            num = 0;
+            ShowPic();
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e) //上一張
+        {
+            num--;
+            if(num < 0)
+            {
+                num = photo.GetUpperBound(0);
+            }
+            ShowPic();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)//下一張
+        {
+            num++;
+            if(num > photo.GetUpperBound(0))
+            {
+                num = 0;
+            }
+            ShowPic();
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)//最末張
+        {
+            num = photo.GetUpperBound(0);
+            ShowPic();
+        }
+
+
+    }
+}
