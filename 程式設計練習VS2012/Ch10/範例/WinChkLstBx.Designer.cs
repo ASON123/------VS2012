b4bce1781dad9377a19077346ef3cdcfb12ec9dc
ASON123 <k10416123@gmail.com>
2015-05-21 13:50:16 +0800

    OK

--- 程式設計練習VS2012/Ch10/範例/WinChkLstBx.Designer.cs
+++ 程式設計練習VS2012/Ch10/範例/WinChkLstBx.Designer.cs
@@ -28,20 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.chkListLot = new System.Windows.Forms.CheckedListBox();
+            this.lblShow = new System.Windows.Forms.ListBox();
+            this.btnCheckLot = new System.Windows.Forms.Button();
+            this.btnRest = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // chkListLot
+            // 
+            this.chkListLot.CheckOnClick = true;
+            this.chkListLot.FormattingEnabled = true;
+            this.chkListLot.Location = new System.Drawing.Point(13, 30);
+            this.chkListLot.Name = "chkListLot";
+            this.chkListLot.Size = new System.Drawing.Size(442, 106);
+            this.chkListLot.TabIndex = 0;
+            this.chkListLot.TabIndexChanged += new System.EventHandler(this.btnCheckLot_Click);
+            this.chkListLot.StyleChanged += new System.EventHandler(this.WinChkLstBx_Load);
+            // 
+            // lblShow
+            // 
+            this.lblShow.FormattingEnabled = true;
+            this.lblShow.ItemHeight = 12;
+            this.lblShow.Location = new System.Drawing.Point(13, 143);
+            this.lblShow.Name = "lblShow";
+            this.lblShow.Size = new System.Drawing.Size(442, 88);
+            this.lblShow.TabIndex = 1;
+            // 
+            // btnCheckLot
+            // 
+            this.btnCheckLot.Location = new System.Drawing.Point(34, 253);
+            this.btnCheckLot.Name = "btnCheckLot";
+            this.btnCheckLot.Size = new System.Drawing.Size(75, 23);
+            this.btnCheckLot.TabIndex = 2;
+            this.btnCheckLot.Text = "對獎";
+            this.btnCheckLot.UseVisualStyleBackColor = true;
+            this.btnCheckLot.Click += new System.EventHandler(this.btnCheckLot_Click);
+            // 
+            // btnRest
+            // 
+            this.btnRest.Location = new System.Drawing.Point(192, 253);
+            this.btnRest.Name = "btnRest";
+            this.btnRest.Size = new System.Drawing.Size(75, 23);
+            this.btnRest.TabIndex = 2;
+            this.btnRest.Text = "清除";
+            this.btnRest.UseVisualStyleBackColor = true;
+            this.btnRest.Click += new System.EventHandler(this.btnRest_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(347, 253);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "離開";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
             // 
             // WinChkLstBx
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(467, 288);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnRest);
+            this.Controls.Add(this.btnCheckLot);
+            this.Controls.Add(this.lblShow);
+            this.Controls.Add(this.chkListLot);
             this.Name = "WinChkLstBx";
             this.Text = "WinChkLstBx";
             this.Load += new System.EventHandler(this.WinChkLstBx_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckedListBox chkListLot;
+        private System.Windows.Forms.ListBox lblShow;
+        private System.Windows.Forms.Button btnCheckLot;
+        private System.Windows.Forms.Button btnRest;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
     }
 }
--- 程式設計練習VS2012/Ch10/範例/WinChkLstBx.cs
+++ 程式設計練習VS2012/Ch10/範例/WinChkLstBx.cs
@@ -17,9 +17,101 @@ namespace 程式設計練習VS2012.Ch10.範例
             InitializeComponent();
         }
 
+        int[] pcLot = new int[6];
+       
         private void WinChkLstBx_Load(object sender, EventArgs e)
         {
-
+            chkListLot.MultiColumn = true;          //水平欄顯示
+            chkListLot.ColumnWidth = 45;            //水平欄寬45
+            for (int i = 1; i < 49; i++ )
+            {
+                chkListLot.Items.Add(i.ToString());
+            }
+            lblShow.Text = "本期未開獎";
         }
+
+        private void btnCheckLot_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            int i = 0;
+            //使用for迴圈看目前勾選幾個按鈕
+            for ( i = 0; i < chkListLot.Items.Count;i++ )
+            {
+                if(chkListLot.GetItemChecked(i))
+                {
+                    count++;
+                }
+            }
+            if (count != 6) //沒有選擇6個按鈕就會離開此函示
+            {
+                MessageBox.Show("請選擇6個號碼");
+                return;
+            }
+            SetLot(ref pcLot, 1, 49, pcLot.Length);
+                Array.Sort(pcLot);
+                string myNumstr = "", pcNumstr = "";
+                for ( i = 0; i <= pcLot.GetUpperBound(0);i++ )
+                {
+                    pcNumstr += pcLot [i].ToString() + ", ";
+                }
+                for ( i = 0; i < chkListLot .Items .Count;i++ )
+                {
+                    if(chkListLot.GetItemChecked(i))
+                    {
+                        myNumstr += chkListLot.Items[i].ToString () + ",";
+                    }
+                }
+
+                //lblShow 顯示本期開獎號碼 
+                lblShow.Text = "本期大樂透號碼如下 \n " + pcNumstr + "\n";
+                //判斷是否中獎
+                if(pcNumstr == myNumstr)
+                {
+                    lblShow.Items.Add("恭喜你中獎了!");
+                }
+                else
+                {
+                    lblShow.Items.Add("沒中...再接再厲!!");
+                }
+        }
+
+        private void SetLot(ref int [] choose,int min ,int max,int num)
+        {
+            int[] lot = new int[50];
+            int max_dim, choice;
+            int i, j;
+            max_dim = max - min + 1;
+            for (i = 0; i < max_dim;i++ )
+            {
+                lot[i] = min + i;
+            }
+            Random ranObj = new Random();
+            for (i = 0; i < num;i++ )
+            {
+                choice = ranObj.Next(0 , max_dim);
+                choose[i] = lot[choice];
+                for (j = choice; j < max_dim; j++ )
+                {
+                    lot[j] = lot[j + 1];
+                }
+                max_dim--;
+            }
+             
+        }
+
+        private void btnRest_Click(object sender, EventArgs e) // 清除
+        {
+            for(int i = 0; i < chkListLot.Items.Count; i ++)
+            {
+                chkListLot.SetItemChecked(i , false);
+
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e) //離開
+        {
+            Application.Exit();
+        }
+
     }
 }
