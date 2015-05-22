18df832146504f5eb9b49f2436c50c4b358c7acd
ASON123 <k10416123@gmail.com>
2015-05-22 16:24:07 +0800

    Ch13 WinKeyPress 完成

---
+++ 程式設計練習VS2012/Ch13/範例/WinKeyPress.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace 程式設計練習VS2012.Ch13
+{
+    partial class WinKeyPress
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtQty = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "產品編號";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "單價";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "數量";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "金額";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(71, 20);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 22);
+            this.txtId.TabIndex = 1;
+            this.txtId.TextChanged += new System.EventHandler(this.txtId_TextChanged);
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(71, 65);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 22);
+            this.txtPrice.TabIndex = 1;
+            this.txtPrice.TextChanged += new System.EventHandler(this.txtPrice_TextChanged);
+            // 
+            // txtQty
+            // 
+            this.txtQty.Location = new System.Drawing.Point(71, 108);
+            this.txtQty.Name = "txtQty";
+            this.txtQty.Size = new System.Drawing.Size(100, 22);
+            this.txtQty.TabIndex = 1;
+            this.txtQty.TextChanged += new System.EventHandler(this.txtPrice_TextChanged);
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(71, 150);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 22);
+            this.txtTotal.TabIndex = 1;
+            this.txtTotal.TextChanged += new System.EventHandler(this.txtPrice_TextChanged);
+            // 
+            // WinKeyPress
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 190);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtQty);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "WinKeyPress";
+            this.Text = "WinKeyPress";
+            this.Load += new System.EventHandler(this.WinKeyPress_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtQty;
+        private System.Windows.Forms.TextBox txtTotal;
+    }
+}
---
+++ 程式設計練習VS2012/Ch13/範例/WinKeyPress.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 程式設計練習VS2012.Ch13
+{
+    public partial class WinKeyPress : Form
+    {
+        public WinKeyPress()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void WinKeyPress_Load(object sender, EventArgs e)
+        {
+            txtPrice.Text = "0"; //單價
+            txtQty.Text = "0";   //數量
+            txtTotal.Text = "0";
+            txtTotal.ReadOnly = true;
+        }
+
+        private void txtId_TextChanged(object sender, EventArgs e) 
+        {
+            int Loc = txtId.SelectionStart; //儲存目前由標位子
+            txtId.Text = txtId.Text.ToUpper();
+            txtId.SelectionStart = Loc; //將遊標位子還原到原來位子
+        }
+        private void txtId_KeyPress (object sender , KeyPressEventArgs e )
+        {
+            if(txtId.Text.Length < 6)
+            {
+                if(txtId.SelectionStart == 0)
+                {
+                    string S = e.KeyChar.ToString().ToUpper();
+                    if(S.CompareTo("A") < 0 || S.CompareTo("Z") > 0)
+                    {
+                        e.Handled = true;
+                    }
+                }
+                else
+                {
+                    if(e.KeyChar.CompareTo('0') < 0 ||e.KeyChar.CompareTo('9') > 0)
+                    {
+                        if(e.KeyChar != '\b')
+                        {
+                            e.Handled = true;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if(e.KeyChar != '\b')
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void txtPrice_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int price = int.Parse(txtId.Text);
+                int qty = int.Parse(txtQty.Text);
+                txtTotal.Text = (price * qty).ToString();
+            }catch
+            {
+            }
+        }
+        private void txtPrice_KeyPass(object sender , KeyPressEventArgs e )
+        {
+            if((e.KeyChar < '0' || e.KeyChar > '9') && (e.KeyChar != '\b'))
+            {
+                e.Handled = true;
+            }
+        }
+        private void txtQty_Text (object sender , EventArgs e )
+        {
+            try
+            {
+                int price = int.Parse(txtPrice.Text);
+                int qty = int.Parse(txtQty.Text);
+                txtTotal.Text = (price * qty).ToString();
+            }catch
+            {
+
+            }
+        }
+        private void txtQty_KeyPass(object sender , KeyPressEventArgs e )
+        {
+            if((e.KeyChar < '0' || e.KeyChar > '9') && (e.KeyChar != '\b'))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
