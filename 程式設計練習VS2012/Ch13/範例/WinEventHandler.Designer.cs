32391db61716890d567f35ff034c8cf8df0e88cf
ASON123 <k10416123@gmail.com>
2015-05-22 16:23:32 +0800

    Ch13 WinEventHandler 完成

---
+++ 程式設計練習VS2012/Ch13/範例/WinEventHandler.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace 程式設計練習VS2012.Ch13.範例
+{
+    partial class WinEventHandler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.lblShow = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 16F);
+            this.label1.Location = new System.Drawing.Point(167, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "共用事件範例";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(13, 95);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "按鈕1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(188, 95);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "按鈕2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(384, 95);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 23);
+            this.btn3.TabIndex = 1;
+            this.btn3.Text = "按鈕3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // lblShow
+            // 
+            this.lblShow.AutoSize = true;
+            this.lblShow.Location = new System.Drawing.Point(25, 203);
+            this.lblShow.Name = "lblShow";
+            this.lblShow.Size = new System.Drawing.Size(33, 12);
+            this.lblShow.TabIndex = 0;
+            this.lblShow.Text = "label1";
+            // 
+            // WinEventHandler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 262);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lblShow);
+            this.Controls.Add(this.label1);
+            this.Name = "WinEventHandler";
+            this.Text = "WinEventHandler";
+            this.Load += new System.EventHandler(this.WinEventHandler_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Label lblShow;
+    }
+}
---
+++ 程式設計練習VS2012/Ch13/範例/WinEventHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 程式設計練習VS2012.Ch13.範例
+{
+    public partial class WinEventHandler : Form
+    {
+        public WinEventHandler()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void WinEventHandler_Load(object sender, EventArgs e)
+        {
+            lblShow.Text = "";
+        }
+       
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            Button btnHit;
+            btnHit = (Button)sender;
+            lblShow.Text = "目前按下 " + btnHit.Text ;
+        }
+    }
+}
