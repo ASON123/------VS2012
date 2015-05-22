191b79db27ea7f2fef6901c75069ff5588d738a3
ASON123 <k10416123@gmail.com>
2015-05-22 16:23:02 +0800

    CH13 WinAPPRemoveEvent 完成

---
+++ 程式設計練習VS2012/Ch13/範例/WinAPPRemoveEvent.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace 程式設計練習VS2012.Ch13.範例
+{
+    partial class WinAPPRemoveEvent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblAns = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(11, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "+";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(158, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(276, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "=";
+            // 
+            // lblAns
+            // 
+            this.lblAns.AutoSize = true;
+            this.lblAns.Location = new System.Drawing.Point(303, 33);
+            this.lblAns.Name = "lblAns";
+            this.lblAns.Size = new System.Drawing.Size(33, 12);
+            this.lblAns.TabIndex = 1;
+            this.lblAns.Text = "label1";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(37, 143);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = " 新增事件";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(158, 143);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "移除事件";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // WinAPPRemoveEvent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 232);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblAns);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "WinAPPRemoveEvent";
+            this.Text = "WinAPPRemoveEvent";
+            this.Load += new System.EventHandler(this.WinAPPRemoveEvent_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblAns;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+    }
+}
---
+++ 程式設計練習VS2012/Ch13/範例/WinAPPRemoveEvent.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 程式設計練習VS2012.Ch13.範例
+{
+    public partial class WinAPPRemoveEvent : Form
+    {
+        public WinAPPRemoveEvent()
+        {
+            InitializeComponent();
+        }
+        private void MyTextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int n1, n2;
+                n1 = int.Parse(textBox1.Text);
+                n2 = int.Parse(textBox2.Text);
+                lblAns.Text = (n1 + n2).ToString();
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+        private void WinAPPRemoveEvent_Load(object sender, EventArgs e)
+        {
+            lblAns.Text = "0";
+            textBox1.TextChanged += new EventHandler(MyTextChanged);
+            textBox2.TextChanged += new EventHandler(MyTextChanged);
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            textBox1.TextChanged += new EventHandler(MyTextChanged);
+            textBox2.TextChanged += new EventHandler(MyTextChanged);
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            textBox1.TextChanged -= new EventHandler(MyTextChanged);
+            textBox2.TextChanged -= new EventHandler(MyTextChanged);
+        }
+
+    }
+}
